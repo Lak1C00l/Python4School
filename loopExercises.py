420eab1672704f6d5910dea531e07c345734067f
Lak1C00l <lara.atanasova@gmail.com>
2025-10-22 09:21:13 -0400

    while loops

--- loopExercises.py
+++ loopExercises.py
@@ -2,21 +2,62 @@
 Lara Atanasova
 Loop exercises
 """
-# Create a program that takes in input from a user and prints out the sum from 1 to whatever number the user puts in
-userNum = int(input("please input a Number one larger than the number you wish to calculate the sum of a series: "));
-sum = 0;
-for i in range(0,userNum):
-    sum = sum + i
-    print(sum);
+#
+rows = int(input("max num of stars: "))
+for i in range(1,rows+1):
+    print("*"*i)
+for i in range(rows-1,0,-1):
+    print("*"*i)
 
-# Create a program that nests for loops that will print out all the prime numbers from 1-100
-# potentialPrime = 0;
-# potPrime = 0;
-# for p in range(0,101):
-#     for x in range(2,101):
-#             potentialPrime = p % x 
-#     if not(potentialPrime > 0) and p != 1:
-#             print(p)
+#find factorial of num enerted by user
+num = int(input("Please enert number: "))
+fact = 1;
+for i in range(num,0,-1):
+    fact*= i
+print(fact)
+
+
+
+userNum = int(input("Please enter number to count: "))
+count = 0
+for digits in str(userNum):
+    count+= 1
+print(count)
+
+# Create a program that will take in a number from the user, then a second number
+# and it will display the multiplication tables of the first number from 1-Second Number
+
+num1 = int(input("Please enter first number: "))
+num2 = int(input("Please enter second number: "))
+
+for i in range(1,num1+1):
+    line = " "
+    for k in range(1,num2+1):
+        times = i +"X" + {k} +" = " +{i*k} +","
+    print(line)
+    
+#Create a program that will take in a users name and then print it out in reverse using for loops
+userName = input("what is your name: ")
+revName =""
+userName
+for i in range(len(userName)-1,-1,-1):
+    revName += userName[i]
+print(revName)
+
+# Create a program that nests for loops that will print out all the prime numbers from 1-10
+
+#Other ways of doing it
+# for i in range(0,numberOfRows+1):
+#     row="*"*i
+#     if row !="":
+#         print(row)
+
+# for i in range(0,numberOfRows+1):
+#     row=""
+#     for k in range(0,i):
+#         row+= "*"
+#     if row !="":
+#         print(row)
 
 #Create a program that will print out a right angle triangle of stars. Number of rows to be taken in as user input
 numberOfRows = int(input("How many rows do you want? : "))
@@ -25,8 +66,14 @@ for i in range(numberOfRows):
         print("*",end=" ")
     print()
 
-#Create a program that will take in a users name and then print it out in reverse using for loops
-userName = input("what is your name: ")
-userName = userName.split()
-for i in reversed(userName):
-    print(i)
+# Create a program that takes in input from a user and prints out the sum from 1 to whatever number the user puts in
+userNum = int(input("please input a Number : "));
+sum = 0;
+step = 1;
+adder = 1;
+if userNum < 0:
+    step = -1
+    adder = -1
+for i in range(0,userNum+adder,step):
+    sum = sum + i
+    print(sum);
---
+++ whileLoops.py
@@ -0,0 +1,122 @@
+import os
+from random import randint
+#nim
+# Print the instructions
+print("The game Nim starts out with seven sticks on the table.")
+print("Each player takes turns picking up 1, 2 or 3 sticks and cannot pass.")
+print("Whoever picks up the last stick loses (the other player wins).")
+
+numberOfSticks = 7;
+computerMove = randint(1,3);
+userMove = input("Would you like to go second or first: ").lower
+while not(numberOfSticks == 0):
+    while userMove == 'first':
+        userSticks = int(input("How many sticks would you like to take between three between one? "))
+        if userSticks != 3 or userSticks != 2 or userSticks != 1:
+            userSticks = int(input("Choose BETWEEN three and one: "))
+        elif (numberOfSticks - userSticks) < 0 :
+            userSticks = int(input(f" Chose a number of sticks that is between {numberOfSticks} and 1 "))
+        else:
+            numberOfSticks - userSticks
+            print(f"Number of sticks left is: {numberOfSticks}")
+    
+        if (numberOfSticks - computerMove) < 0 :
+            randint(1,3)
+        else:
+            numberOfSticks -= computerMove
+            print(f"Number of sticks left is: {numberOfSticks}")
+
+    while userMove == 'second':
+        numberOfSticks -= 2
+        userSticks = int(input("How many sticks would you like to take between three between one? "))
+        if userSticks != 3 or userSticks != 2 or userSticks != 1:
+            userSticks = int(input("Choose BETWEEN three and one: "))
+        elif (numberOfSticks - userSticks) < 0 :
+            userSticks = int(input(f" Chose a number of sticks that is between {numberOfSticks} and 1 "))
+        else:
+            numberOfSticks - userSticks
+            print(f"Number of sticks left is: {numberOfSticks}")
+
+
+        if (numberOfSticks - computerMove) < 0 :
+            randint(1,3)
+        else:
+            numberOfSticks -= computerMove
+            print(f"Number of sticks left is: {numberOfSticks}")
+    
+        
+   
+
+# accepts any number of grades. When the user enters “exit” terminate the loop and calculate the average, 
+# printing the average to the screen
+calculateing = True;
+while calculateing:
+    grades = []
+    while exit != "exit":
+        grade = float(input("Please input your grade: "))
+        grades.append(grade)
+        exit = input("Say exit if you would like to calculate your average: ").lower();
+
+    average = 0;
+    for i in grades:
+        average = i + average
+        numberOfElements = len(grades)
+        average = average/numberOfElements
+        calculateing = False;
+    print(average)
+
+#accepts a sequence of lines (blank line to terminate to exit) as input 
+# and prints the lines as output (all characters in lower case)
+ex = False 
+finailMesssage = ''
+while not ex:
+    lines = input("Please give a series of sentences (nothing to exit).").lower
+    finailMesssage += lines + "\r\n"
+    if line == "":
+        ex = True
+print(finailMesssage)
+
+
+
+#iterates the integers from 1 to 50. 
+# For multiples of three print "Fizz" instead of the number 
+# and for the multiples of five print "Buzz". 
+# For numbers which are multiples of both three and five print "FizzBuzz"
+for i in range(1,51):
+    if i%3 ==0 and i%5 == 0:
+        print("FizzBuzz")
+    elif i%5 == 0:
+        print("Buzz")
+    elif i%3 ==0 :
+        print("Fizz")
+    else:
+        print(i);
+count = 1;
+while count<=50:
+    if count%3 ==0 and count%5 == 0:
+        print("FizzBuzz")
+    elif count%5 == 0:
+        print("Buzz")
+    elif count%3 ==0 :
+        print("Fizz")
+    else:
+        print(i);
+    
+
+    count += 1;
+
+# Create a game where one user enters a number and the other  user guesses a number from, and the program tells them if its too high or low. Game ends when the user selected the correct number
+userNum1 = int(input("Please enter number to be guessed between 1 and 1000: "))
+clear = lambda: os.system('cls')
+userNum2 = int(input("Please enter your guess: "))
+os.system('cls')
+while userNum1<1 or userNum1>1000:
+    userNum1 = int(input("Please enter number to be guessed that is between 1 and 1000: "))
+while userNum1 != userNum2:
+        if userNum2 > userNum1:
+            print("Number is too high")
+            userNum2 = int(input("Please enter your guess: "))
+        elif userNum2 < userNum1:
+            print("Number is too small")
+            userNum2 = int(input("Please enter your guess: "))
+print("Congratulations!")
